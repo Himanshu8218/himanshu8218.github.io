92d0bd601b4a7122e84bdc86c4313eaceca795a6
himanshu8218 <tyagihimanshu801@gmail.com>
2020-06-03 15:17:40 +0530

    First Release

---
+++ precache-manifest.5bf7202e9c3ec34de7f85221fb8d03b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703ff4abeaf9788022e0253a8171eca2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1805bcc1deb6c479aae0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e8cd8db58eb4b539ce1",
+    "url": "/static/js/2.3967d565.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3967d565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1805bcc1deb6c479aae0",
+    "url": "/static/js/main.05faa43c.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
